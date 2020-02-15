1e91fe18de8694854066d306feb9cfe57935e0b2
Maciel Fidelis <macielf1994@gmail.com>
2020-02-14 21:52:18 -0300

    Lista 1 de exercicios

---
+++ atividade-1-2.js
@@ -0,0 +1,7 @@
+var user = require("readline-sync")
+Numero=user.question("Insira um numero para verificação de ímpar ou par: ")
+numero %2===0
+console.log ("Numero é par")
+
+    if numero %1===1
+    console.log ("Numero é impar")
