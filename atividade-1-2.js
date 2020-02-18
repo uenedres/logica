2d9a424f6a58c82fed77aa82aa93b393e55e5920
uenedres <wueernedreyzz@hotmail.com>
2020-02-17 21:48:36 -0300

    Lista Basica de atividades

--- atividade-1-2.js
+++ atividade-1-2.js
@@ -1,7 +1,7 @@
 var user = require("readline-sync")
-Numero=user.question("Insira um numero para verificação de ímpar ou par: ")
-numero %2===0
-console.log ("Numero é par")
-
-    if numero %1===1
-    console.log ("Numero é impar")
+var Numero=user.question("Insira um numero para verificação de ímpar ou par: ")
+if((Numero % 2) == 0){
+    console.log("Numero é par")
+ }else{
+    console.log("Numero é impar")
+ }
---
+++ atividade-1-3.js
@@ -0,0 +1,10 @@
+var user=require ("readline-sync")
+var pergunta=user.question("Insira a senha do usuário: ")
+
+if (pergunta=1234) {
+    
+    console.log("Acesso Permitido")
+
+} else {
+    console.log("Acesso Negado")
+}
