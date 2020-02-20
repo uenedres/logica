394f2580a19414c5ffcce6c7896be952a5a1477e
uenedres <wueernedreyzz@hotmail.com>
2020-02-20 20:48:13 -0300

    lista

--- atividade-1-11.js
+++ atividade-1-11.js
@@ -1,10 +1,11 @@
 var user=require("readline-sync")
-var nota1=user.question("Insira sua nota 1:")
-var nota2=user.question("Insira sua nota 2:")
-var nota3=user.question("Insira sua nota 3:")
-var media=((nota1+nota2+nota3)/3)
+var nota1=user.questionFloat("Insira sua nota 1:")
+var nota2=user.questionFloat("Insira sua nota 2:")
+var nota3=user.questionFloat("Insira sua nota 3:")
+var media=(nota1+nota2+nota3)/3
 
 if (media>=7){
     console.log("ALUNO APROVADO")
+}else if (media<7){
+    console.log("ALUNO REPROVADO")
 }
-    
